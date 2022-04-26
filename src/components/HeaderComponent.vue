8eeddc19369f0e2cf943c6a902ae19792396d250
Maria Stachie <mary.stachie@gmail.com>
2022-04-26 17:10:26 +0200

    HeaderComponent done

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -8,7 +8,16 @@
           alt="logo_img"
         />
       </div>
-      <div class="col-9 header-nav"></div>
+      <div class="col-7 header-nav">
+        <ul>
+          <li v-for="element in arrayNav" :key="element">
+            <a href="#">{{ element }}</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-2">
+        <button class="btn-orange">GET QUOTE!</button>
+      </div>
     </div>
   </div>
 </template>
@@ -16,11 +25,44 @@
 <script>
 export default {
   name: "HeaderComponent",
+  data() {
+    return {
+      arrayNav: ["HOME", "ABOUT", "SERVICES", "WORK", "ARTICLES"],
+    };
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+.container-fluid {
+  padding: 0;
+}
 .img-logo {
-  width: 100px;
+  width: 170px;
+  padding: 10px;
+}
+.header-nav ul {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+}
+
+.header-nav li {
+  list-style: none;
+  flex: 0 0 auto;
+  margin: 15px;
+
+  a {
+    color: #666;
+    text-decoration: none;
+    font-size: 8px;
+  }
+  &:hover {
+    color: #fed23f;
+    border-bottom: 2px solid #fed23f;
+  }
+}
+.btn-orange {
+  margin-top: 37px;
 }
 </style>
