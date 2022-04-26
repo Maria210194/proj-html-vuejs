1768cef00db31a8d8581ccad76033671eb55b289
Maria Stachie <mary.stachie@gmail.com>
2022-04-26 16:17:41 +0200

    add images

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Header</div>
+  <div class="container-fluid w-90">
+    <div class="d-flex row">
+      <div class="col-3">
+        <img
+          class="img-logo"
+          src="@/assets/images/construction_logo_retina.png"
+          alt="logo_img"
+        />
+      </div>
+      <div class="col-9 header-nav"></div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,4 +20,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.img-logo {
+  width: 100px;
+}
 </style>
